4fb169de327c9cb75b00e43387c5abde072ed639
Chris Ohlmacher <55671489+cjohlmacher@users.noreply.github.com>
2021-12-02 15:31:38 -0800

    add environ variable

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 from flask import Flask, render_template, redirect, flash, jsonify, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
-
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "placeholder"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',"placeholder")
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
